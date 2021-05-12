f9e1269aa4581d9ded0a7524126a7626bdace1e6
Matthias Kainer <mail@matthias-kainer.de>
2021-05-12 08:56:07 +0200

    Switched to rollup to provide systemjs and module files

---
+++ rollup.config.js
@@ -0,0 +1,73 @@
+import { terser } from "rollup-plugin-terser";
+import resolve from "rollup-plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import typescript from "rollup-plugin-typescript2";
+import filesize from "rollup-plugin-filesize";
+import minifyHTML from "rollup-plugin-minify-html-literals";
+
+import pkg from "./package.json";
+
+const input = `./src/index.ts`;
+const plugins = [
+    typescript(),
+    replace({ "Reflect.decorate": "undefined" }),
+    minifyHTML(),
+    commonjs(),
+    resolve(),
+    json(),
+    terser({
+        module: true,
+        warnings: true,
+        mangle: {
+            properties: {
+                regex: /^__/,
+            },
+        },
+    }),
+    filesize({
+        showBrotliSize: true,
+    }),
+];
+const external = ["lit-element"]
+
+const get = (input, pkg) => {
+    const mainBundle = {
+        input,
+        external,
+        output: {
+            file: pkg.module,
+            format: "esm",
+            sourcemap: true,
+        },
+        plugins,
+    };
+
+    const cjsBundle = {
+        input,
+        external,
+        output: {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: true,
+        },
+        plugins,
+    };
+
+    const systemBundle = {
+        input,
+        external,
+        output: {
+            file: pkg.system,
+            format: "system",
+            sourcemap: true,
+        },
+        plugins,
+    };
+    return [mainBundle, cjsBundle, systemBundle]
+}
+export default [
+    ...get(input, pkg), 
+    ...get(`./src/full/index.ts`, pkg.full)
+];
