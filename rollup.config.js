dacef8fbc220fef41761c66ef5c94162dc865a97
Matthias Kainer <mail@matthias-kainer.de>
2023-11-07 15:01:20 +0100

    Updated dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { terser } from "rollup-plugin-terser";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
